ed4e280c5bba9223a1fa5cb8a343ca7928be1300
Brandon <Brandonjorgen@gmail.com>
2021-11-11 10:42:54 -0700

    wow look at all these maybe useless build files I can't get rid of :)

---
+++ CavalryProject/Intermediate/Source/CavalryProject.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class CavalryProject : ModuleRules
+{
+	public CavalryProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
